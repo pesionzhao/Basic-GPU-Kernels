210df40c67b4005184538d0c8e4928bd1bf6ff9a
PesionWorker <2357394024@qq.com>
2025-04-21 14:55:14 +0000

    flashattention without divide sqrt(d)

--- flashattention/FA.cuh
+++ flashattention/FA.cuh
@@ -39,41 +39,11 @@ __device__ float warpReduce(float val){
     return val;
 }
 
+// 每次读取k,v的Bc行都从全局内存中取
 template<int BLOCK_SIZE, int HEAD_DIM>
-__global__ void flashAttentionGEMMV1Kernel(const float* q, const float* k, const float* v, float* dst, int batch, int num_head, int seq_len, int head_dim){
+__global__ void flashAttentionKernel(const float* q, const float* k, const float* v, float* dst, int batch, int num_head, int seq_len, int head_dim){
     //假设Q/K/V shape [batch, num_head, seq_len, head_dim]
-    //block [block_size, head_dim], grid[M/block_size, bc*num_head]
-    int bid = blockIdx.x*BLOCK_SIZE;
-    int row = bid+threadIdx.y;
-    int col = threadIdx.x;
-    __shared__ float local_O[Br][Bc];
-    int num_pack = (seq_len + Bc - 1)/Bc;
-    const float* intputK = k+blockIdx.y*seq_len*head_dim;
-    const float* intputV = v+blockIdx.y*seq_len*head_dim;
-    __shared__ float intputQ[Br*HEAD_DIM];
-    intputQ[threadIdx.y*HEAD_DIM+col] = q[blockIdx.y*seq_len*head_dim+row*HEAD_DIM+col];//固定Q
-    __syncthreads();
-    for(int pack_id = 0; pack_id<num_pack; pack_id++){
-        float qk_res = 0.0f;
-        for(int i = 0; i<head_dim; i++){
-            // qk_res += intputQ[threadIdx.y*HEAD_DIM+i]*intputK[(col%Bc+pack_id*Bc)*HEAD_DIM+i];// Q*K^T
-            qk_res += intputQ[threadIdx.y*HEAD_DIM+i]*intputK[col%Bc+pack_id*Bc+seq_len*i];// Q * K
-        }
-        local_O[threadIdx.y][col%Bc]=qk_res;
-        intputQ[threadIdx.y*HEAD_DIM+col]=0.0f;
-        __syncthreads();
-        for(int i = 0; i<Bc;i++){
-            intputQ[threadIdx.y*HEAD_DIM+col]+=local_O[threadIdx.y][i]*intputV[(i+pack_id*Bc)*HEAD_DIM+col];
-        }
-        __syncthreads();
-    }
-    dst[blockIdx.y*seq_len*head_dim+row*HEAD_DIM+col] = intputQ[threadIdx.y*HEAD_DIM+col];
-};
-
-template<int BLOCK_SIZE, int HEAD_DIM>
-__global__ void flashAttentionV1Kernel(const float* q, const float* k, const float* v, float* dst, int batch, int num_head, int seq_len, int head_dim){
-    //假设Q/K/V shape [batch, num_head, seq_len, head_dim]
-    //block [block_size, head_dim], grid[M/block_size, bc*num_head]
+    //block [head_dim, block_size], grid[M/block_size, bc*num_head]
     int bid = blockIdx.x*BLOCK_SIZE;
     int row = bid+threadIdx.y;
     int col = threadIdx.x;
@@ -91,10 +61,10 @@ __global__ void flashAttentionV1Kernel(const float* q, const float* k, const flo
     for(int pack_id = 0; pack_id<num_pack; pack_id++){
         float qk_res = 0.0f;
         for(int i = 0; i<head_dim; i++){
+            //由于x方向线程数为head_dim, 大于bc, 在进行乘法时只需要bc个线程参与计算即可，这里使用求余防止分支发散，x方向上，每个bc块的元素都一样
             qk_res += intputQ[threadIdx.y*HEAD_DIM+i]*intputK[(pack_id*Bc+col%Bc)*HEAD_DIM+i];// Q*K^T
             // qk_res += intputQ[threadIdx.y*HEAD_DIM+i]*intputK[col%Bc+pack_id*Bc+seq_len*i];// Q * K
         }
-        //由于x方向线程数为head_dim, 大于bc, 在进行乘法时只需要bc个线程参与计算即可，这里使用求余防止分支发散，x方向上，每个bc块的元素都一样
         float cur_max = qk_res;
         // online softmax
         cur_max = warpReduce<MaxOp, Bc>(qk_res);
@@ -116,25 +86,60 @@ __global__ void flashAttentionV1Kernel(const float* q, const float* k, const flo
     dst[blockIdx.y*seq_len*head_dim+row*HEAD_DIM+col] = outputO[threadIdx.y*HEAD_DIM+col];
 };
 
-template<int BLOCK_SIZE>
-__global__ void GEMM(const float* src1, const float* src2, const float* dst, int M, int N, int K){
-    int row = blockDim.y*blockIdx.y+threadIdx.y;
-    int col = blockDim.x*blockIdx.x+threadIdx.x;
-    int tid = threadIdx.y*blockDim.y+threadIdx.x;
-    __shared__ float sa[BLOCK_SIZE*BLOCK_SIZE];
-    __shared__ float sb[BLOCK_SIZE*BLOCK_SIZE];
-    int num_tile = (K+BLOCK_SIZE-1)/BLOCK_SIZE;
-    float tmp = 0;
-    for(int i = 0; i<num_tile; i++){
-        sa[tid] = src1[row*K+i*BLOCK_SIZE+threadIdx.x];
-        sa[tid] = src1[(i*BLOCK_SIZE+threadIdx.y)*N+col];
-        __syncthreads();
-        for(int k = 0; k<BLOCK_SIZE; k++){
-            tmp += sa[threadIdx.y*BLOCK_SIZE+k]*sb[k*BLOCK_SIZE+threadIdx.x];
+// 将每次使用的Bc行k,v存入共享内存，再进行计算，未达到加速效果。。。
+template<int BLOCK_SIZE, int HEAD_DIM>
+__global__ void flashAttention_shared_Kernel(const float* q, const float* k, const float* v, float* dst, int batch, int num_head, int seq_len, int head_dim){
+    //假设Q/K/V shape [batch, num_head, seq_len, head_dim]
+    //block [head_dim, block_size], grid[M/block_size, bc*num_head]
+    int bid = blockIdx.x*BLOCK_SIZE;
+    int row = bid+threadIdx.y;
+    int col = threadIdx.x;
+    __shared__ float local_O[Br][Bc];
+    int num_pack = (seq_len + Bc - 1)/Bc;
+    float old_max = 0;
+    float old_d = 0;
+    __shared__ float intputK[Bc*HEAD_DIM+1];
+    __shared__ float intputV[Bc*HEAD_DIM+1];
+    __shared__ float intputQ[Br*HEAD_DIM];
+    __shared__ float outputO[Br*HEAD_DIM];
+    outputO[threadIdx.y*HEAD_DIM+col] = 0.0f;
+    intputQ[threadIdx.y*HEAD_DIM+col] = q[blockIdx.y*seq_len*head_dim+row*HEAD_DIM+col];//固定Q
+    __syncthreads();
+    for(int pack_id = 0; pack_id<num_pack; pack_id++){
+        float qk_res = 0.0f;
+        int r = threadIdx.y;
+        for(int r = threadIdx.y; r<Bc; r+=blockDim.y){
+            intputK[r*HEAD_DIM+col] = k[blockIdx.y*seq_len*head_dim+(pack_id*Bc+r)*HEAD_DIM+col];
+            intputV[r*HEAD_DIM+col] = v[blockIdx.y*seq_len*head_dim+(pack_id*Bc+r)*HEAD_DIM+col];
+            __syncthreads();
         }
+        for(int i = 0; i<head_dim; i++){
+            //由于x方向线程数为head_dim, 大于bc, 在进行乘法时只需要bc个线程参与计算即可，这里使用求余防止分支发散，x方向上，每个bc块的元素都一样
+            qk_res += intputQ[threadIdx.y*HEAD_DIM+i]*intputK[(col%Bc)*HEAD_DIM+i];// Q*K^T
+            // qk_res += intputQ[threadIdx.y*HEAD_DIM+i]*intputK[col%Bc+pack_id*Bc+seq_len*i];// Q * K
+        }
+        float cur_max = qk_res;
+        // online softmax
+        cur_max = warpReduce<MaxOp, Bc>(qk_res);
+        cur_max = max(cur_max, old_max);
+        float cur_sum_tmp = __expf(qk_res-cur_max);
+        local_O[threadIdx.y][col%Bc]=cur_sum_tmp; // 不用同步，因为每个线程操作对应的shared memory
+        float cur_sum = warpReduce<SumOp, Bc>(cur_sum_tmp); //只有Br行Bc的线程有用
+        float alpha = __expf(old_max-cur_max);
+        float new_d = alpha*old_d+cur_sum;
+        local_O[threadIdx.y][col%Bc]/=new_d;
+        outputO[threadIdx.y*HEAD_DIM+col] = outputO[threadIdx.y*HEAD_DIM+col]*old_d/new_d*alpha;
+        __syncthreads();
+        for(int i = 0; i<Bc;i++){
+            outputO[threadIdx.y*HEAD_DIM+col]+=local_O[threadIdx.y][i]*intputV[(i)*HEAD_DIM+col];
+        }
+        old_max = cur_max;
+        old_d = new_d;
+        __syncthreads(); //这里必须同步，否则可能下一次循环开始更改intputV的值，这里还没算完
     }
-    dst[row*N+col] = tmp;
-}
+    dst[blockIdx.y*seq_len*head_dim+row*HEAD_DIM+col] = outputO[threadIdx.y*HEAD_DIM+col];
+};
+
 
 template<int HEAD_DIM>
 void lauchFA(const float* q, const float* k, const float* v, float* dst, int batch, int num_head, int seq_len, int head_dim=64){
@@ -142,6 +147,7 @@ void lauchFA(const float* q, const float* k, const float* v, float* dst, int bat
     int grid_size_y = batch*num_head;
     int grid_size_x = (seq_len+Br-1)/Br;
     dim3 block(HEAD_DIM, Br);
-    flashAttentionV1Kernel<Br, HEAD_DIM><<<grid_size_x, block>>>(q,k,v,dst,batch,num_head,seq_len,head_dim);
+    dim3 grid(grid_size_x, grid_size_y);
+    flashAttentionKernel<Br, HEAD_DIM><<<grid, block>>>(q,k,v,dst,batch,num_head,seq_len,head_dim);
     CHECK_KERNEL();
 }
--- flashattention/check.cu
+++ flashattention/check.cu
@@ -3,15 +3,20 @@
 #include"FA.cuh"
 #include"gemm.cuh"
 #include"rand.h"
-void compare(float* a, float* b, int M, int N){
+#include <chrono>
+void compare(float* a, float* b, int batch, int num_head, int seq_len, int head_dim){
     int cnt = 0;
-    for(int i = 0; i<M; i++){
-        for(int j = 0; j<N; j++){
-            if(abs(a[i*N+j]-b[i*N+j])>1e-6){
-                printf("error occurs in[%d][%d], cpu_ans = %f, fa = %f\n", i, j, a[i*N+j], b[i*N+j]);
-                cnt++;
-                if (cnt==32)
-                    return;
+    for(int bc = 0; bc<batch*num_head; bc++){
+        float* a1 = a+bc*seq_len*head_dim;
+        float* b1 = b+bc*seq_len*head_dim;
+        for(int i = 0; i<seq_len; i++){
+            for(int j = 0; j<head_dim; j++){
+                if(abs(a1[i*head_dim+j]-b1[i*head_dim+j])>1e-6){
+                    printf("%d bc error occurs in[%d][%d], cpu_ans = %f, fa = %f\n", bc, i, j, a1[i*head_dim+j], b1[i*head_dim+j]);
+                    cnt++;
+                    if (cnt==32)
+                        return;
+                }
             }
         }
     }
@@ -19,39 +24,46 @@ void compare(float* a, float* b, int M, int N){
     printf("result in[0][0] is %f\n", a[200]);
     return;
 }
-void attention(float* Q, float* K, float* V, float* dst,  int seq_len, int head_dim){
-    //q*k^T
-    float* O = new float[seq_len*seq_len]();
-    for(int i = 0; i<seq_len; i++){
-        for(int j = 0; j<seq_len; j++){
-            for(int k = 0; k<head_dim; k++){
-                // O[i*seq_len+j] += Q[i*head_dim+k]*K[k*seq_len+j]; //q*k
-                O[i*seq_len+j] += Q[i*head_dim+k]*K[k+head_dim*j]; //q*k^T
+void attention(float* Q, float* K, float* V, float* dst,  int batch, int num_head, int seq_len, int head_dim){
+    float* O = new float[seq_len*seq_len];
+    for(int bc = 0; bc<batch*num_head; bc++){
+        memset(O, 0, seq_len*seq_len*sizeof(float));
+        float* Q1 = Q+bc*seq_len*head_dim;
+        float* K1 = K+bc*seq_len*head_dim;
+        float* V1 = V+bc*seq_len*head_dim;
+        float* dst1 = dst+bc*seq_len*head_dim;
+        //q*k^T
+        for(int i = 0; i<seq_len; i++){
+            for(int j = 0; j<seq_len; j++){
+                for(int k = 0; k<head_dim; k++){
+                    // O[i*seq_len+j] += Q[i*head_dim+k]*K[k*seq_len+j]; //q*k
+                    O[i*seq_len+j] += Q1[i*head_dim+k]*K1[k+head_dim*j]; //q*k^T
+                }
             }
         }
-    }
-    //softmax
-    for(int i = 0; i<seq_len; i++){
-        float max_v = -100;
-        float sum_v = 0.0;
-        for(int j = 0; j<seq_len; j++){
-            max_v = max(max_v, O[i*seq_len+j]);
+        //softmax
+        for(int i = 0; i<seq_len; i++){
+            float max_v = -100;
+            float sum_v = 0.0;
+            for(int j = 0; j<seq_len; j++){
+                max_v = max(max_v, O[i*seq_len+j]);
+            }
+            for(int j = 0; j<seq_len; j++){
+                O[i*seq_len+j] = exp(O[i*seq_len+j]-max_v);
+                sum_v += O[i*seq_len+j];
+            }
+            for(int j = 0; j<seq_len; j++){
+                O[i*seq_len+j] /= sum_v;
+            }
+            // printf("%d row max = %f, sum = %f\n", i, max_v, sum_v);
+            // printf("%d row O[row][0] = %f\n", i, O[i*seq_len]);
         }
-        for(int j = 0; j<seq_len; j++){
-            O[i*seq_len+j] = exp(O[i*seq_len+j]-max_v);
-            sum_v += O[i*seq_len+j];
-        }
-        for(int j = 0; j<seq_len; j++){
-            O[i*seq_len+j] /= sum_v;
-        }
-        // printf("%d row max = %f, sum = %f\n", i, max_v, sum_v);
-        // printf("%d row O[row][0] = %f\n", i, O[i*seq_len]);
-    }
-    //O*V
-    for(int i = 0; i<seq_len; i++){
-        for(int j = 0; j<head_dim; j++){
-            for(int k = 0; k<seq_len; k++){
-                dst[i*head_dim+j] += O[i*seq_len+k]*V[k*head_dim+j];
+        //O*V
+        for(int i = 0; i<seq_len; i++){
+            for(int j = 0; j<head_dim; j++){
+                for(int k = 0; k<seq_len; k++){
+                    dst1[i*head_dim+j] += O[i*seq_len+k]*V1[k*head_dim+j];
+                }
             }
         }
     }
@@ -60,47 +72,58 @@ void attention(float* Q, float* K, float* V, float* dst,  int seq_len, int head_
 int main(){
     mt19937_state rng;
     manual_seed(&rng, 1142);
-    const int M = 64;
-    const int N = 32;
-    float* q = (float*)malloc(M*N*sizeof(float));
-    float* k = (float*)malloc(M*N*sizeof(float));
-    float* v = (float*)malloc(M*N*sizeof(float));
-    float* dst = (float*)malloc(M*N*sizeof(float));
-    float* fadst = (float*)malloc(M*N*sizeof(float));
-    uniform_(q, M*N, 0, 1, &rng);
-    uniform_(k, M*N, -0, 1, &rng);
-    uniform_(v, M*N, -0, 1, &rng);
+    int repeat = 100;
+    const int batch = 16;
+    const int num_head = 16;
+    const int seq_len = 64;
+    const int head_dim = 32;
+    float* q = (float*)malloc(batch*num_head*seq_len*head_dim*sizeof(float));
+    float* k = (float*)malloc(batch*num_head*seq_len*head_dim*sizeof(float));
+    float* v = (float*)malloc(batch*num_head*seq_len*head_dim*sizeof(float));
+    float* dst = (float*)malloc(batch*num_head*seq_len*head_dim*sizeof(float));
+    float* fadst = (float*)malloc(batch*num_head*seq_len*head_dim*sizeof(float));
+    uniform_(q, batch*num_head*seq_len*head_dim, 0, 1, &rng);
+    uniform_(k, batch*num_head*seq_len*head_dim, -0, 1, &rng);
+    uniform_(v, batch*num_head*seq_len*head_dim, -0, 1, &rng);
     printf("random number is%f\n", q[0]);
     printf("random number is%f\n", k[0]);
     printf("random number is%f\n", v[0]);
-    for(int i = 0; i<M; i++){
-        for(int j = 0; j<N; j++){
-            dst[i*N+j] = 0;
-            fadst[i*N+j] = 0;
+    for(int i = 0; i<seq_len; i++){
+        for(int j = 0; j<head_dim; j++){
+            dst[i*head_dim+j] = 0;
+            fadst[i*head_dim+j] = 0;
         }
     }
     float* dq, *dk, *dv, *res, *fares;
-    cudaMalloc((void**)&dq, M*N*sizeof(float));
-    cudaMalloc((void**)&dk, M*N*sizeof(float));
-    cudaMalloc((void**)&dv, M*N*sizeof(float));
-    cudaMalloc((void**)&res, M*N*sizeof(float));
-    cudaMalloc((void**)&fares, M*N*sizeof(float));
-    cudaMemcpy(dq, q, M*N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(dk, k, M*N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(dv, v, M*N*sizeof(float), cudaMemcpyHostToDevice);
-    lauchFA<N>(dq, dk, dv, fares, 1,1, M, N);
-    cudaMemcpy(fadst, fares, M*N*sizeof(float), cudaMemcpyDeviceToHost);
-    // launchGEMMv2Kernel<float>(dq, dk, res, M, N, N);
-    // cudaMemcpy(dq, res, M*N*sizeof(float), cudaMemcpyDeviceToDevice);
-    // launchGEMMv2Kernel<float>(dq, dv, res, M, N, N);
+    cudaMalloc((void**)&dq, batch*num_head*seq_len*head_dim*sizeof(float));
+    cudaMalloc((void**)&dk, batch*num_head*seq_len*head_dim*sizeof(float));
+    cudaMalloc((void**)&dv, batch*num_head*seq_len*head_dim*sizeof(float));
+    cudaMalloc((void**)&res, batch*num_head*seq_len*head_dim*sizeof(float));
+    cudaMalloc((void**)&fares, batch*num_head*seq_len*head_dim*sizeof(float));
+    cudaMemcpy(dq, q, batch*num_head*seq_len*head_dim*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(dk, k, batch*num_head*seq_len*head_dim*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(dv, v, batch*num_head*seq_len*head_dim*sizeof(float), cudaMemcpyHostToDevice);
+    lauchFA<head_dim>(dq, dk, dv, fares, batch, num_head, seq_len, head_dim);
+    auto start = std::chrono::high_resolution_clock::now();
+    for(int i = 0; i<repeat; i++){
+        lauchFA<head_dim>(dq, dk, dv, fares, batch, num_head, seq_len, head_dim);
+    }
+    cudaDeviceSynchronize();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed.count() << " ms" << std::endl;
+    cudaMemcpy(fadst, fares, batch*num_head*seq_len*head_dim*sizeof(float), cudaMemcpyDeviceToHost);
+    // launchGEMMv2Kernel<float>(dq, dk, res, seq_len, head_dim, head_dim);
+    // cudaMemcpy(dq, res, seq_len*head_dim*sizeof(float), cudaMemcpyDeviceToDevice);
+    // launchGEMMv2Kernel<float>(dq, dv, res, seq_len, head_dim, head_dim);
     // cudaFree(dq);
     // cudaFree(dk);
     // cudaFree(dv);
-    // cudaMemcpy(dst, res, M*N*sizeof(float), cudaMemcpyDeviceToHost);
+    // cudaMemcpy(dst, res, seq_len*head_dim*sizeof(float), cudaMemcpyDeviceToHost);
     // cudaFree(res);
     // cudaFree(fares);
-    attention(q, k, v, dst, M, N);
-    compare(dst, fadst, M, N);
+    attention(q, k, v, dst, batch, num_head, seq_len, head_dim);
+    compare(dst, fadst, batch, num_head, seq_len, head_dim);
     free(q);
     free(k);
     free(v);
